783c3f0343833c0482039b5e83d7a5c2a565b3ac
Geun-ni <wjdrms2798@hanyang.ac.kr>
2025-11-12 22:01:04 +0900

    시간표 기능 js코드

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "abcde12345", // ← 여기에 실제 MySQL 비밀번호 입력
+  password: "abcde12345", 
   database: "hy_timetable",
   waitForConnections: true,
   connectionLimit: 10,
