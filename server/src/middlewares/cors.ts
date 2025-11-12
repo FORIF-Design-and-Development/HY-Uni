c32b6cfd2db408957e25462f72cc5e07c0df7ca4
Geun-ni <wjdrms2798@hanyang.ac.kr>
2025-11-12 21:52:52 +0900

    remove old .ts files

--- server/src/middlewares/cors.ts
+++
@@ -1,9 +0,0 @@
-import cors from 'cors';
-import type { CorsOptions } from 'cors';
-
-const corsOptions: CorsOptions = {
-  origin: process.env.CORS_ORIGINS?.split(',') ?? '*',
-  credentials: true,
-};
-
-export const corsMiddleware = cors(corsOptions);
