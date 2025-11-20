995dfa7339fa6b57c2bf7f4dfdb919665aec528a
RAKRAK21 <94699316+RAKRAK21@users.noreply.github.com>
2025-11-20 16:44:03 +0900

    feat/campus-seats-2 시설 현황 크롤링 결과 임시로 dashboard에 구현. 추후에 수정

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard';
 import ExamplePage from './pages/ExamplePage';
-import RegisterTest from './pages/auth/RegisterTest';
 import LoginTest from './pages/auth/LoginTest';
+import RegisterTest from './pages/auth/RegisterTest';
 import Timetable from './pages/timetable/Timetable';
 
 export default function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/example">예시</Link>
+        <Link to="/">dashboard</Link>
       </nav>
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/example" element={<ExamplePage />} />
         <Route path="/register" element={<RegisterTest/>}/>
         <Route path="/login" element={<LoginTest />}/>
